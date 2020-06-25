f7c2cec560554ec5a165d68bafb24a5ab9b7ac2f
iMax <45369969+MaxWalker979@users.noreply.github.com>
2020-06-25 23:27:54 +0200

    Main Patch

---
+++ __resource.lua
@@ -0,0 +1,19 @@
+  
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+client_script "ui.lua"
+server_script "imax.lua"
+
+ui_page "html/index.html"
+files {
+    'html/index.html',
+    'html/js/index.js',
+    'html/fonts/karmatic_arcade/ka1.woff2',
+    'html/fonts/karmatic_arcade/ka1.woff',
+    'html/css/basestyle.css',
+    'html/css/reset.css',
+    'html/css/lists.css',
+    'html/images/background.png',
+    'html/images/avocat.png',
+    'html/images/imax.png'
+}
---
+++ imax.lua
@@ -0,0 +1,12 @@
+Citizen.CreateThread(function ()
+    while true do
+        Citizen.Wait(0)
+        print('')
+        print('###################')
+        print('# rapport_systeme #')
+        print('#   Created  By   #')
+        print('#    iMax#0705    #')
+        print('###################')
+        break
+    end
+end)
